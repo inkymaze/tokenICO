f2b60535fd06b27c2eec983758a96f36bd97a363
inkymaze <nickwcannon@gmail.com>
2017-08-09 06:53:41 -0700

    reconfig bundles

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 module.exports = {
   entry: './src/entry.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+  
+      filename: './index_bundle.js'
   },
   module: {
     loaders: [
